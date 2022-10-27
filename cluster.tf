bfe1cd8dec7ea62e31499d694c0f8148cf589697
mvpc-11232 <mvp2612@hotmail.com>
2022-10-27 07:03:14 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,3 +1,6 @@
+
+
+
 # # Creates Redis Cluster
 # resource "aws_elasticache_cluster" "redis" {
 #   cluster_id           = "roboshop-${var.ENV}"
@@ -57,38 +60,3 @@
 #     Name = "roboshop-redis-sg-${var.ENV}"
 #   }
 # }
-
-
-
-
-
-
-# # # Creates DocDB Cluster
-# # resource "aws_docdb_cluster" "docdb" {
-# #   cluster_identifier      = "roboshop-${var.ENV}"
-# #   engine                  = "docdb"
-# #   master_username         = "admin1"
-# #   master_password         = "roboshop1"
-# # # True only during lab, in prod , we will take a snapshot and that time value will be false
-# #   skip_final_snapshot     = true
-# #   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
-# # }
-
-
-# # # Creates Subnet Group
-# # resource "aws_docdb_subnet_group" "docdb" {
-# #   name       = "roboshop-mongo-${var.ENV}"
-# #   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
-
-# #   tags = {
-# #     Name = "My docdb subnet group"
-# #   }
-# # }
-
-# # # Creats DocDB Cluster Instances and adds them to the cluster
-# # resource "aws_docdb_cluster_instance" "cluster_instances" {
-# #   count              = 1
-# #   identifier         = "roboshop-${var.ENV}"
-# #   cluster_identifier = aws_docdb_cluster.docdb.id
-# #   instance_class     = "db.t3.medium"
-# # }
