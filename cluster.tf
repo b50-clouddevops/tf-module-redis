1040f66c9b66f3425ad98ba30168b7d74146a8a1
mvpc-11232 <mvp2612@hotmail.com>
2022-10-27 06:14:30 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -1,29 +1,41 @@
-# Creates DocDB Cluster
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "roboshop-${var.ENV}"
-  engine                  = "docdb"
-  master_username         = "admin1"
-  master_password         = "roboshop1"
-# True only during lab, in prod , we will take a snapshot and that time value will be false
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+# Creates Redis Cluster
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "redis-${var.ENV}"
+  engine               = ""
+  node_type            = "cache.m4.large"
+  num_cache_nodes      = 2
+  parameter_group_name = "default.memcached1.4"
+  port                 = 11211
 }
 
 
-# Creates Subnet Group
-resource "aws_docdb_subnet_group" "docdb" {
-  name       = "roboshop-mongo-${var.ENV}"
-  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
 
-  tags = {
-    Name = "My docdb subnet group"
-  }
-}
+# # Creates DocDB Cluster
+# resource "aws_docdb_cluster" "docdb" {
+#   cluster_identifier      = "roboshop-${var.ENV}"
+#   engine                  = "docdb"
+#   master_username         = "admin1"
+#   master_password         = "roboshop1"
+# # True only during lab, in prod , we will take a snapshot and that time value will be false
+#   skip_final_snapshot     = true
+#   db_subnet_group_name    = aws_docdb_subnet_group.docdb.name
+# }
 
-# Creats DocDB Cluster Instances and adds them to the cluster
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 1
-  identifier         = "roboshop-${var.ENV}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
-}
+
+# # Creates Subnet Group
+# resource "aws_docdb_subnet_group" "docdb" {
+#   name       = "roboshop-mongo-${var.ENV}"
+#   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+#   tags = {
+#     Name = "My docdb subnet group"
+#   }
+# }
+
+# # Creats DocDB Cluster Instances and adds them to the cluster
+# resource "aws_docdb_cluster_instance" "cluster_instances" {
+#   count              = 1
+#   identifier         = "roboshop-${var.ENV}"
+#   cluster_identifier = aws_docdb_cluster.docdb.id
+#   instance_class     = "db.t3.medium"
+# }
