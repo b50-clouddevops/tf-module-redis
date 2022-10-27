9744156a0fee02862f9e51de97bcaf1762369b15
mvpc-11232 <mvp2612@hotmail.com>
2022-10-27 06:27:06 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -22,8 +22,32 @@ resource "aws_elasticache_subnet_group" "subnet-group" {
 }
 
 # Creates Security Group for Redis
+resource "aws_security_group" "allow_redis" {
+  name        = "roboshop-redis-${var.ENV}"
+  description = "roboshop-redis-${var.ENV}"
+  vpc_id      = ???
 
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 
 
 
