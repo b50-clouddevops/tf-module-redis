2785c90aa6f8cd9ceba9cf0b7db029e850373cbb
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 06:12:13 +0530

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,7 +1,9 @@
-resource "aws_route53_record" "redis" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "redis-${var.ENV}.data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME"
-  type    = "CNAME"
-  ttl     = 10
-  records = [aws_elasticache_cluster.redis.cluster_address]
-}
+# resource "aws_route53_record" "redis" {
+#   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
+#   name    = "redis-${var.ENV}.data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME"
+#   type    = "CNAME"
+#   ttl     = 10
+#   records = [aws_elasticache_cluster.redis.cluster_address]
+# }
+
+output "redis-output"
