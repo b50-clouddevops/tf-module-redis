63a3ebdd4d2488dd275172f53b57a0987a466078
mvpc-11232 <mvp2612@hotmail.com>
2022-10-31 07:00:21 +0530

    Auto Push Commit

--- cluster.tf
+++ cluster.tf
@@ -6,7 +6,7 @@ resource "aws_elasticache_cluster" "redis" {
   num_cache_nodes      = 1                      # An ideal prod-cluster should have 3 nodes.
   parameter_group_name = aws_elasticache_parameter_group.default.name
   engine_version       = "6.x"
-  port                 = 6379
+  port                 = var.ELASTICCACHE_PORT
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = [aws_security_group.allow_redis.id]
 }
@@ -31,15 +31,15 @@ resource "aws_security_group" "allow_redis" {
 
   ingress {
     description      = "Allow Redis Connection From Default VPC"
-    from_port        = 6379
-    to_port          = 6379
+    from_port        = var.ELASTICCACHE_PORT
+    to_port          = var.ELASTICCACHE_PORT
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
   }
 
   ingress {
     description      = "Allow Redis Connection From Private VPC"
-    from_port        = 6379
+    from_port        = var.ELASTICCACHE_PORT
     to_port          = 6379
     protocol         = "tcp"
     cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
